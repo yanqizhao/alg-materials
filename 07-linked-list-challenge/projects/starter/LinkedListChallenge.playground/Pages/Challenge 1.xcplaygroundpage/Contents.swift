4c23e064266e1dac7326aca54082623e351d6593
yanqizhao <zhaoyanqi703@hellobike.com>
2022-04-28 21:03:26 +0800

    feat: linked list challenge 1

--- 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 1.xcplaygroundpage/Contents.swift
+++ 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 1.xcplaygroundpage/Contents.swift
@@ -7,7 +7,20 @@
  Create a function that prints the nodes of a linked list in reverse order.
  */
 func printInReverse<T>(_ list: LinkedList<T>) {
-  
+    printInReverse(list.head)
 }
 
+private func printInReverse<T>(_ node: Node<T>?) {
+    guard let node = node else { return }
+    printInReverse(node.next)
+    print(node.value)
+}
+
+example(of: "Reverse LinkedList") {
+    var list = LinkedList<Int>()
+    list.append(1)
+    list.append(2)
+    list.append(3)
+    printInReverse(list)
+}
 //: [Next Challenge](@next)
