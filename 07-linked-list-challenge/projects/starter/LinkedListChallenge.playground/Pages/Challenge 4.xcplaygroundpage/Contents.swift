4759dac3a8e3c7fc7ef36940c758be697b611bb4
yanqizhao <zhaoyanqi703@hellobike.com>
2022-04-29 17:36:33 +0800

    feat: linked list challenge 4

--- 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 4.xcplaygroundpage/Contents.swift
+++ 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 4.xcplaygroundpage/Contents.swift
@@ -8,8 +8,79 @@
  */
 func mergeSorted<T: Comparable>(_ left: LinkedList<T>,
                                 _ right: LinkedList<T>) -> LinkedList<T> {
-  
-  LinkedList<T>() // placeholder
+    // 左边空，返回右边
+    guard !left.isEmpty else {
+        return right
+    }
+    
+    // 右边空，返回左边
+    guard !right.isEmpty else {
+        return left
+    }
+    
+    var newHead: Node<T>?
+    var tail: Node<T>?
+    
+    var currentLeft = left.head
+    var currentRight = right.head
+    
+    // 找到头结点
+    if let leftNode = currentLeft, let rightNode = currentRight {
+        if leftNode.value < rightNode.value {
+            newHead = leftNode
+            currentLeft = leftNode.next
+        } else {
+            newHead = rightNode
+            currentRight = rightNode.next
+        }
+        tail = newHead
+    }
+    
+    // 按顺序后移尾结点
+    while let leftNode = currentLeft, let rightNode = currentRight {
+        if leftNode.value < rightNode.value {
+            tail?.next = leftNode
+            currentLeft = leftNode.next
+        } else {
+            tail?.next = rightNode
+            currentRight = rightNode.next
+        }
+        tail = tail?.next
+    }
+    
+    // 如果左边还有结点就继续
+    if let leftNodes = currentLeft {
+        tail?.next = leftNodes
+    }
+    
+    // 如果右边还有结点就继续
+    if let rightNodes = currentRight {
+        tail?.next = rightNodes
+    }
+    
+    var list = LinkedList<T>()
+    list.head = newHead
+    list.tail = {
+        // tail 还没有移到链表的尾部
+        while let next = tail?.next {
+            tail = next
+        }
+        return tail
+    }()
+    
+    return list
 }
 
+example(of: "Merge two lists") {
+    var list = LinkedList<Int>()
+    list.append(4)
+    list.append(9)
+    list.append(17)
+
+    var list2 = LinkedList<Int>()
+    list2.append(3)
+    list2.append(10)
+    list2.append(12)
+    print(mergeSorted(list2, list))
+}
 //: [Next Challenge](@next)
