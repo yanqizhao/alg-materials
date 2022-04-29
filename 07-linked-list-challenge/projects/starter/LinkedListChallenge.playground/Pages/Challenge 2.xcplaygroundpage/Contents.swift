33c1453b7e117b1ea1fb7f169e7d50c46eac6a1a
yanqizhao <zhaoyanqi703@hellobike.com>
2022-04-29 15:26:13 +0800

    feat: linked list challenge 2

--- 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 2.xcplaygroundpage/Contents.swift
+++ 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 2.xcplaygroundpage/Contents.swift
@@ -7,7 +7,21 @@
  Create a function that finds the middle node of a linked list.
  */
 func getMiddle<T>(_ list: LinkedList<T>) -> Node<T>? {
-  nil // placeholder 
+    let count = list.count / 2
+    var node = list.head
+    for _ in 0..<count {
+        node = node?.next
+    }
+    return node
 }
 
+example(of: "Find the middle node") {
+    var list = LinkedList<Int>()
+    list.append(1)
+    list.append(2)
+    list.append(3)
+    list.append(4)
+    list.append(5)
+    print(getMiddle(list) as Any)
+}
 //: [Next Challenge](@next)
