2ce61184d375fb6742077691104ea0c7148f5862
yanqizhao <zhaoyanqi703@hellobike.com>
2022-04-29 17:53:12 +0800

    feat: feat: linked list challenge 5 没有考虑前几个相同值的结点

--- 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 5.xcplaygroundpage/Contents.swift
+++ 07-linked-list-challenge/projects/starter/LinkedListChallenge.playground/Pages/Challenge 5.xcplaygroundpage/Contents.swift
@@ -8,8 +8,38 @@
  */
 
 extension LinkedList where Value: Equatable {
-  
   mutating func removeAll(_ value: Value) {
-
+      if head?.value == value {
+          head = head?.next
+      }
+      
+      var prevNode = self.head
+      var currentNode = self.head?.next
+      
+      while currentNode != nil {
+          if currentNode!.value == value {
+              prevNode!.next = currentNode!.next
+          } else {
+              prevNode = currentNode
+          }
+          currentNode = currentNode!.next
+      }
+      tail = prevNode
   }
+}
+
+example(of: "Remove all occurrences of a specific element") {
+    var list = LinkedList<Int>()
+    list.append(4)
+    list.append(4)
+    list.append(4)
+    list.append(17)
+    list.append(4)
+    list.append(9)
+    list.append(17)
+    list.append(4)
+    list.append(9)
+    list.append(17)
+    list.removeAll(17)
+    print(list)
 }
